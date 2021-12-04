6cbee7d0b83fa5e79ad6d0ed90ae8e8d996af373
shubham409 <sm091274@gmail.com>
2021-12-04 17:01:04 +0530

    Spelling Mistake

--- users/views.py
+++ users/views.py
@@ -38,7 +38,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request,f' your accoun has been updated ')
+            messages.success(request,f' your account has been updated ')
             return redirect('user-profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
